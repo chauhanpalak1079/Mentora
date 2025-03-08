7ac9cf70e3cda8c31a665f468a5ce160a993c8b9
Palak Chauhan <eatizsipam@gmail.com>
2025-03-08 22:32:20 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fucking_do_it_pr0prely")  # Change this in production
     SESSION_TYPE = "filesystem"  # Session storage type
     DATABASE_NAME = "mentora.db"  # SQLite database
-    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyBcfVY6OI_pQdROE-CCgMb1hLgKXukWya0")  # Gemini API key
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "...")  # Gemini API key
 
 class DevelopmentConfig(Config):
     DEBUG = True  # Enable debugging
